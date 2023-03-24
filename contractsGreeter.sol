102e9de6e3afdf9243472fe802ac41eee52ff2e3
Avilansky <128795057+Avilansky@users.noreply.github.com>
2023-03-24 13:34:36 +0300

    Create contractsGreeter.sol

---
+++ contractsGreeter.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Greeter {
+    string private greeting;
+    constructor(string memory _greeting) {
+        console.log("deploying a greeter",_greeting);
+        greeting = _greeting;
+    }
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+}
